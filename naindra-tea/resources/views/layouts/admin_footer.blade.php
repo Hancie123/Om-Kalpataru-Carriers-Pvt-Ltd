82383b4e3c23ca157c9404d2f6c0063e02dbb77a
Hancie123 <hanciewanemphago@gmail.com>
2023-07-19 19:08:05 +0545

    commit change

--- naindra-tea/resources/views/layouts/admin_footer.blade.php
+++ naindra-tea/resources/views/layouts/admin_footer.blade.php
@@ -8,9 +8,7 @@
 <script src="{{asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('assets/js/sidebarmenu.js')}}"></script>
 <script src="{{asset('assets/js/app.min.js')}}"></script>
-<script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
 <script src="{{asset('assets/libs/simplebar/dist/simplebar.js')}}"></script>
-<script src="../assets/js/dashboard.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css">
@@ -18,4 +16,3 @@
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
-<script src="https://unpkg.com/htmx.org@1.9.2"></script>
--- naindra-tea/resources/views/layouts/admin_header.blade.php
+++ naindra-tea/resources/views/layouts/admin_header.blade.php
@@ -2,8 +2,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @stack('title')
+    <script src="{{asset('assets/nepali.datepicker.v4.0.1.min.js')}}"
+        type="text/javascript"></script>
+    <link href="{{asset('assets/nepali.datepicker.v4.0.1.min.css')}}"
+        rel="stylesheet" type="text/css" />
     <link rel="shortcut icon" type="image/png" href="{{url('assets/Images/2.png')}}" />
     <link rel="stylesheet" href="{{url('/assets/css/styles.min.css')}}" />
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
@@ -11,10 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.18/dist/sweetalert2.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <script src="http://nepalidatepicker.sajanmaharjan.com.np/nepali.datepicker/js/nepali.datepicker.v4.0.1.min.js"
-        type="text/javascript"></script>
-    <link href="http://nepalidatepicker.sajanmaharjan.com.np/nepali.datepicker/css/nepali.datepicker.v4.0.1.min.css"
-        rel="stylesheet" type="text/css" />
+  
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
  
